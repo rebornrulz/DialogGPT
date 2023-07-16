514cf26d05830190565277fe266c9d777acd0a71
Reborn Rulz <77287878+rebornrulz@users.noreply.github.com>
2023-07-17 07:53:18 +0800

    Create ChatCompletion.py

---
+++ ChatCompletion.py
@@ -0,0 +1,7 @@
+completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "What are some famous astronomical observatories?"}
+  ]
+)
